a85c9ca69c023ad0ecf38d3940a854d9df474506
Max Mulatz <klappradla@gmail.com>
2015-05-18 00:15:17 +0200

    add store controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,26 @@
-var app = angular.module('store', [ ]);
+'use strict';
+var app = angular.module('geometryStore', [ ]);
+
+var gems = [
+  {
+    name: 'triangle',
+    price: 2.95,
+    description: 'A triangle is a polygon with three edges and three vertices.',
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name: 'square',
+    price: 3.95,
+    description: 'In geometry, a square is a regular quadrilateral, which means that it has four equal sides and four equal angles (90-degree angles, or right angles).',
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name: 'circle',
+    price: 4.95,
+    description: 'A circle is a simple shape of Euclidean geometry that is the set of all points in a plane that are at a given distance from a given point, the centre.',
+    canPurchase: true,
+    soldOut: false
+  }
+];
